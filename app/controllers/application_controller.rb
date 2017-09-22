5e88d65a3e7be976258cedf44ed8df79d317f0cc
Cecily2 <cecily.downs@gmail.com>
2017-09-22 09:23:13 -0700

    Add index route and helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,23 @@ class ApplicationController < Sinatra::Base
     enable :sessions
     set :session_secret, "password_security"
   end
-  
+
+  get "/" do
+    if logged_in?
+      redirect "/books"
+    else
+      erb :index
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
 end
