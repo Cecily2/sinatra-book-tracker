cd62a87bd3a34fba0ad54235f16e9bd15a3a0052
Cecily2 <cecily.downs@gmail.com>
2017-09-22 12:53:43 -0700

    Books can be added by title or isbn. ISBN is stored in database.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,7 +17,7 @@ class BooksController < ApplicationController
   end
 
   post "/books" do
-    url = "https://www.googleapis.com/books/v1/volumes?q=intitle:#{params[:title]}&key=#{ENV['API_KEY']}"
+    url = "https://www.googleapis.com/books/v1/volumes?q=#{params[:search_term]}&key=#{ENV['API_KEY']}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
     parsed_response = JSON.parse(response)
@@ -28,7 +28,8 @@ class BooksController < ApplicationController
     new_book.name = book_data["title"]
     new_book.author = book_data["authors"][0]
     new_book.cover = book_data["imageLinks"]["thumbnail"]
-    
+    new_book.isbn = book_data["industryIdentifiers"][0]["identifier"]
+
     new_book.user_id = current_user.id
 
     if params["read"] == "on"
