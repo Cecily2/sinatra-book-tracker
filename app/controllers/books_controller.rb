8a6536e1bb16e4860a6d3bf344c2a1de0a8054d0
Cecily2 <cecily.downs@gmail.com>
2017-09-22 11:38:41 -0700

    Add book post route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,25 @@ class BooksController < ApplicationController
   end
 
   post "/books" do
+    url = "https://openlibrary.org/api/books?bibkeys=ISBN:#{params[:isbn]}&format=json&jscmd=data"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    parsed_response = JSON.parse(response)
+
+    new_book = Book.new(isbn: params[:isbn])
+    new_book.name = parsed_response["ISBN:#{params[:isbn]}"]["title"]
+    new_book.author = parsed_response["ISBN:#{params[:isbn]}"]["authors"][0]["name"]
+    new_book.cover = parsed_response["ISBN:#{params[:isbn]}"]["cover"]["large"]
+    
+    new_book.user_id = current_user.id
+
+    if params["read"] == "on"
+      new_book.read = true
+      new_book.rating = params["rating"].to_i
+      new_book.comments = params["comments"]
+      new_book.date_read = Time.now
+    end
+
   end
 
   get "/books/:id" do
