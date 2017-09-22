d1f80cf91eef37ea9f5d66f37bd4b99e264b3fd5
Cecily2 <cecily.downs@gmail.com>
2017-09-22 09:07:23 -0700

    Create migrations

---
+++ db/migrate/20170922155900_create_books.rb
@@ -0,0 +1,14 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.integer :isbn
+      t.string :name
+      t.string :author
+      t.string :cover
+      t.integer :rating
+      t.string :comments
+      t.boolean :read
+      t.datetime :date_read
+    end
+  end
+end
