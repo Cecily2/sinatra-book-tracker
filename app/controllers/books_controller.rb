0f790e30e1fb92bc4062c68cd81644ff5dd945b3
Cecily2 <cecily.downs@gmail.com>
2017-09-22 09:29:10 -0700

    Add books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,33 @@
+class BooksController < ApplicationController
 
+  get "/books" do
+    if logged_in?
+      erb :'books/books'
+    else
+      redirect "/login"
+    end
+  end
 
-# I can check if a value has changed by
-# comparing the info in params to the info in database
+  get "/books/new" do
+    if logged_in?
+      erb :'books/new'
+    else
+      redirect "/login"
+    end
+  end
+
+  post "/books" do
+  end
+
+  get "/books/:id" do
+  end
+
+  get "/books/:id/edit" do
+  end
+
+  patch "/books/:id" do
+    # I can check if a value has changed by
+    # comparing the info in params to the info in database
+  end
+
+end
