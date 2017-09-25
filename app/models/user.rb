e5650baad6c33833ae8cc1d63f6153baf977bfad
Cecily2 <cecily.downs@gmail.com>
2017-09-24 18:35:54 -0700

    Check for valid email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :books
   validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true  
+  validates :email, :username, uniqueness: true 
+  validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "format not valid" } 
   has_secure_password
 end
